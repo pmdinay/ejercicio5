aaa98179672c48b8fcaedd704e94c67a10acf91a
Pablo Medina Ysasi <pmedinay@alumnos.nebrija.es>
2021-10-21 16:59:43 +0200

    Version inicial y unica

---
+++ main.py
@@ -0,0 +1,30 @@
+""" Cálculo de media,mediana y varianza """
+
+numero1 = float(input("Numero 1: "))
+numero2 = float(input("Numero 2: "))
+numero3 = float(input("Numero 3: "))
+
+lista = [numero1, numero2, numero3]
+def mean(datos):
+    return sum(datos) / len(datos)
+
+print("Media de la lista: ", mean(lista))
+
+def median(data):
+    data = sorted(data)
+    n = len(data)
+    if n == 0:
+        raise StatisticsError("no median for empty data")
+    if n % 2 == 1:
+        return data[n // 2]
+    else:
+        i = n // 2
+        return (data[i - 1] + data[i]) / 2
+
+print("Mediana: ", median(lista))
+
+import numpy
+varianza = numpy.var(lista)
+print("Varianza: ", varianza)
+
+
